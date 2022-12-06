448ab43f31100cd66c74f0b06bf064bdfd6ddeb2
Keagan Hilliard <keagan.hilliard@gmail.com>
2022-12-06 10:55:50 -0700

    Oops

--- src/day6/day6.rs
+++ src/day6/day6.rs
@@ -4,12 +4,10 @@ use std::{
 };
 
 pub fn problem1() {
-    let contents = fs::read_to_string("src/day6/input.txt").expect("Should have found a file");
     println!("Day 6, Problem 1: {}", get_start(4));
 }
 
 pub fn problem2() {
-    let contents = fs::read_to_string("src/day6/input.txt").expect("Should have found a file");
     println!("Day 6, Problem 2: {}", get_start(14));
 }
 
@@ -21,7 +19,6 @@ fn get_start(limit: usize) -> usize {
         let len = char_set.len();
         if len == limit {
             if len == HashSet::<char>::from_iter(char_set.iter().copied()).len() {
-                println!("Time elapsed: {:?}", now.elapsed());
                 return i + 1;
             }
             char_set.pop_front().unwrap();
